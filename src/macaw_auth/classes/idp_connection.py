7d9764daadf633a12c469c588a23d907c4b41d84
Travis Samuel <samuelt@hss.edu>
2024-04-26 17:56:04 -0400

    added limited support for Symantec VIP MFA

--- src/macaw_auth/classes/idp_connection.py
+++ src/macaw_auth/classes/idp_connection.py
@@ -1,6 +1,7 @@
 import requests
 from requests_ntlm import HttpNtlmAuth #pip install requests-ntlm
 from bs4 import BeautifulSoup #pip install beautifulsoup4
+from getpass import getpass
 from urllib.parse import urlparse
 import re
 import sys
@@ -15,16 +16,15 @@ class SAMLAssertion:
         self.__username = username
         self.__password = password
         self.session = requests.Session()
-        self.make_saml_request(self.session)
+        self.make_saml_request()
         if self.auth_type == 'web_form':
             self.create_web_form_payload()
-            self.authenticate_to_web_form(self.session)
+            self.authenticate_to_web_form()
         self.get_saml_assertion()
 
-    def make_saml_request(self, session):
-        session = session
+    def make_saml_request(self):
         if self.auth_type == 'ntlm':
-            session.auth = HttpNtlmAuth(self.__username, self.__password, session)
+            self.session.auth = HttpNtlmAuth(self.__username, self.__password, self.session)
             feature = "html.parser"
         elif self.auth_type == 'web_form':
             # feature = "lxml"
@@ -32,7 +32,7 @@ class SAMLAssertion:
         else:
             message = "Incorrect authorization type provided. Valid types are 'ntlm' or 'web_form'"
             ##RAISE ERROR - TODO
-        self.response = session.get(self.identity_url, verify=self.ssl_verification)
+        self.response = self.session.get(self.identity_url, verify=self.ssl_verification)
         self._redirect_url = self.response.url
         self.__soup = BeautifulSoup(self.response.text, features=feature)
 
@@ -58,7 +58,7 @@ class SAMLAssertion:
 
         self.__payload = payload
 
-    def authenticate_to_web_form(self, session):
+    def authenticate_to_web_form(self):
         for inputtag in self.__soup.find_all(re.compile('(FORM|form)')):
             action = inputtag.get('action')
             loginid = inputtag.get('id')
@@ -67,11 +67,12 @@ class SAMLAssertion:
                 self._redirect_url = parsedurl.scheme + "://" + parsedurl.netloc + action
 
         # Performs the submission of the IdP login form with the above post data
-        response = session.post(
+        response = self.session.post(
             self._redirect_url, data=self.__payload, verify=self.ssl_verification)
         self.__soup = BeautifulSoup(response.text, features="html.parser")
 
     def get_saml_assertion(self):
+        mfa_enabled = False
         assertion = ''
 
         # Look for the SAMLResponse attribute of the input tag (determined by
@@ -80,13 +81,49 @@ class SAMLAssertion:
             if(inputtag.get('name') == 'SAMLResponse'):
                 #print(inputtag.get('value'))
                 assertion = inputtag.get('value')
+                break
+            if('vip' in inputtag.get('name')):
+                mfa_enabled = True
 
-        # Better error handling is required for production use.
         if (assertion == ''):
-            #TODO: Insert valid error checking/handling
-            print('Response did not contain a valid SAML assertion')
-            sys.exit(0)
+            if mfa_enabled:
+                self.authenticate_with_mfa()
+                for inputtag in self.__soup.find_all('input'):
+                    if(inputtag.get('name') == 'SAMLResponse'):
+                        assertion = inputtag.get('value')
+                        break
+            else:
+                #TODO: Insert valid error checking/handling
+                print('Response did not contain a valid SAML assertion')
+                sys.exit(0)
         self.assertion = assertion
 
         # Debug only
         # print(base64.b64decode(assertion))
+
+    def get_vip_code(self):
+        self.__vipcode = getpass(prompt='Enter your Symantec VIP security code: ')
+        try:
+            int(self.__vipcode)
+        except ValueError as err:
+            print('ERROR: Code must be a number. Try again...')
+            self.get_vip_code()
+
+    def authenticate_with_mfa(self):
+        self.get_vip_code()
+        payload = {}
+
+        for inputtag in self.__soup.find_all('input'):
+            name = inputtag.get('name','')
+            value = inputtag.get('value','')
+            # Locate VIP code fields in web form by searching for "[security_]code"
+            if "code" in name.lower():
+                payload[name] = self.__vipcode
+            else:
+                # Keep existing value
+                payload[name] = value
+
+        # Performs the submission of the Symantec VIP code
+        response = self.session.post(
+            self._redirect_url, data=payload, verify=self.ssl_verification)
+        self.__soup = BeautifulSoup(response.text, features="html.parser")
