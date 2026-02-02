650b0e57073680fb6b70f00e5dc147d3833deb11
Travis Samuel <samuelt@hss.edu>
2026-02-02 17:15:44 -0500

    update pytest to match updated UserValidation class attributes

--- tests/test_usernames.py
+++ tests/test_usernames.py
@@ -7,55 +7,54 @@ class TestUsernameValidation:
     @staticmethod
     def validate_user(
             username : str,
-            expected_result : bool = False # Most tests are exceptions
+            expect_pass : bool = True,
+            error_regex : str = None
             ) -> None:
-        user = UsernameValidation(username)
-        if expected_result:
-            assert user.validity
-        elif not expected_result:
-            assert not user.validity
-
-    def expect_valid_result(self, username : str,) -> None:
-        self.validate_user(username, expected_result=True)
-
-    def expect_exception(self, username : str, exception_regex : str) -> None:
-        with pytest.raises(InvalidUsernameError, match=exception_regex):
-            self.validate_user(username, expected_result=False)
+        with pytest.raises(InvalidUsernameError, match=error_regex):
+            try:
+                UsernameValidation(username)
+                if not expect_pass:
+                    pytest.fail("Expected InvalidUsernameError was not raised")
+            except InvalidUsernameError as e:
+                if expect_pass:
+                    pytest.fail(f"Unexpected InvalidUsernameError raised: {e.message}")
+                else:
+                    pass
 
     # Email Tests
     def test_valid_email(self):
-        self.expect_valid_result("fake@example.com")
+        self.validate_user("fake@example.com")
 
     def test_invalid_email_symbols(self):
-        self.expect_exception("user@equ=als.edu", True,  "contains invalid symbol")
+        self.validate_user("user@equ=als.edu", False, "contains invalid symbol")
 
     def test_invalid_email_multiple_ats(self):
-        self.expect_exception("user@hello@fake.com", True,  "User name should contain a single '@' symbol")
+        self.validate_user("user@hello@fake.com", False, "User name should contain a single '@' symbol")
 
     def test_invalid_email_non_alphanum_start_prefix(self):
-        self.expect_exception(".invalid@hello.net", True, "does not start with an alphanumeric character")
+        self.validate_user(".invalid@hello.net", False, "does not start with an alphanumeric character")
 
     def test_invalid_email_non_alphanum_start_domain(self):
-        self.expect_exception("notright@+ultra.ua", True, "does not start with an alphanumeric character")
+        self.validate_user("notright@+ultra.ua", False, "does not start with an alphanumeric character")
 
     def test_invalid_email_non_alphanum_end_prefix(self):
-        self.expect_exception("invalid+@hello.net", True, "does not end with an alphanumeric character")
+        self.validate_user("invalid+@hello.net", False, "does not end with an alphanumeric character")
 
     def test_invalid_email_non_alphanum_end_domain(self):
-        self.expect_exception("notright@ultra.ua+", True, "does not end with an alphanumeric character")
+        self.validate_user("notright@ultra.ua+", False, "does not end with an alphanumeric character")
 
     def test_invalid_email_alphanum_after_symbol(self):
-        self.expect_exception("hello@dot.+com", True, "Username contains consecutive symbols")
+        self.validate_user("hello@dot.+com", False, "Username contains consecutive symbols")
 
     def test_invalid_email_short_domain(self):
-        self.expect_exception("short@xy.z", True, "less than 2")
+        self.validate_user("short@xy.z", False, "less than 2")
 
     # User Tests
     def test_valid_user(self):
         self.expect_valid_result("fakeusername")
 
     def test_invalid_user_invalid_symbol(self):
-        self.expect_exception("user1!", False, "contains invalid symbol")
+        self.validate_user("user1!", False, "contains invalid symbol")
 
     def test_invalid_user_alphanum_after_symbol(self):
-        self.expect_exception("invalid+=user", False, "Username contains consecutive symbols")
+        self.validate_user("invalid+=user", False, "Username contains consecutive symbols")
