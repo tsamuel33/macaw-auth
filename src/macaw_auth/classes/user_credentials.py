4640dd26d8a392dfee65f25350f955b48c3c07eb
Travis Samuel <tsamuel@alum.mit.edu>
2026-08-27 21:08:27 -0400

    Update formatting via Ruff

--- src/macaw_auth/classes/user_credentials.py
+++ src/macaw_auth/classes/user_credentials.py
@@ -1,9 +1,12 @@
 # from . import errors
 # Keyring may require importing dbus-python to work on Linux
-# dbus-python may not install properly via pip, so may need to disable keyring on linux
-import keyring
+# dbus-python may not install properly via pip; maybe disable keyring on Linux
 from getpass import getpass
+
+import keyring
+
 from macaw_auth.classes.idp_connection import SAMLAssertion
+
 
 class UserCredentials:
     """
@@ -26,7 +29,15 @@ class UserCredentials:
     # Set constant values
     keyring_service_name = "AWSUserCredentials"
 
-    def __init__(self, username, identity_url, auth_type, ssl_verification=True, reset_password=False, enable_keyring=False):
+    def __init__(
+        self,
+        username,
+        identity_url,
+        auth_type,
+        ssl_verification=True,
+        reset_password=False,
+        enable_keyring=False,
+    ):
         self.username = username
         self.enable_keyring = enable_keyring
         if enable_keyring.lower() == "true":
@@ -35,31 +46,39 @@ class UserCredentials:
             if not self._password_stored:
                 self.set_keyring_password(getpass())
             elif self._password_stored and reset_password:
-                print('Resetting password.')
+                print("Resetting password.")
                 self.set_keyring_password(getpass())
             self.__password = self.get_keyring_password()
         else:
             self.__password = getpass()
-        self.assertion = SAMLAssertion(self.username, self.__password, identity_url, auth_type, ssl_verification).assertion
+        self.assertion = SAMLAssertion(
+            self.username,
+            self.__password,
+            identity_url,
+            auth_type,
+            ssl_verification,
+        ).assertion
 
     def get_keyring_password(self):
         if self.enable_keyring:
-            print('Getting password from keyring...')
+            print("Getting password from keyring...")
             creds = keyring.get_password(
-                self.keyring_service_name, self.username)
+                self.keyring_service_name, self.username
+            )
             return creds
 
     def check_if_password_stored(self):
         result = self.get_keyring_password()
         if result is None:
-            print('Password not found.')
+            print("Password not found.")
             password_stored = False
         else:
-            print('Stored password found.')
+            print("Stored password found.")
             password_stored = True
         return password_stored
 
     def set_keyring_password(self, password):
         if self.enable_keyring:
             keyring.set_password(
-                self.keyring_service_name, self.username, password)
+                self.keyring_service_name, self.username, password
+            )
