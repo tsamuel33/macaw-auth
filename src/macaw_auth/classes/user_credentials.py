1bf9250e7207c6025c4da309bb76ae8cf156e871
Travis Samuel <tsamuel@alum.mit.edu>
2026-08-30 15:16:47 -0400

    Add docstrings for UserCredentials class and methods

--- src/macaw_auth/classes/user_credentials.py
+++ src/macaw_auth/classes/user_credentials.py
@@ -10,34 +10,41 @@ from macaw_auth.classes.idp_connection import SAMLAssertion
 
 class UserCredentials:
     """
-    A class to represent a user's credentials for authenticating to AWS
-
-    ...
+    Represents a user's credentials for authenticating to AWS
 
     Attributes
-    ----------
-    username : str
-        username for authentication
-    enable_keyring : bool
-        boolean determining if keyring should be used to save password
-    reset_password : bool
-        determines if saved password should be overwritten
-    password_stored : bool
-        boolean stating if the user's password is saved in keyring
+        keyring_service_name (str): Name of the service within keyring that
+            stores the user's credentials. Set to "AWSUserCredentials".
+        username (str): username for authentication
+        enable_keyring (bool): boolean determining if keyring should be used
+            to save password
+        assertion (SAMLAssertion): SAML assertion details after login attempt
     """
 
-    # Set constant values
     keyring_service_name = "AWSUserCredentials"
 
     def __init__(
         self,
-        username,
+        username: str,
         identity_url,
         auth_type,
         ssl_verification=True,
         reset_password=False,
         enable_keyring=False,
     ):
+        """
+        Construct the attributes of the UserCredentials object
+
+        Arguments:
+            username (str): user's login name
+            identity_url (str): url used to log in to AWS
+            auth_type (str): whether authentication is via web form or ntlm
+            ssl_verification (bool): verify identity URL's SSL certificate
+            reset_password (bool): determines if saved password should be
+                overwritten
+            enable_keyring (bool): determines if password is retrieved from
+                keyring
+        """
         self.username = username
         self.enable_keyring = enable_keyring
         if enable_keyring.lower() == "true":
@@ -60,6 +67,10 @@ class UserCredentials:
         ).assertion
 
     def get_keyring_password(self):
+        """
+        Retrieve's password from keyring
+
+        """
         if self.enable_keyring:
             print("Getting password from keyring...")
             creds = keyring.get_password(
@@ -68,6 +79,13 @@ class UserCredentials:
             return creds
 
     def check_if_password_stored(self):
+        """
+        Check's if user's password is stored in keyring
+
+        Returns:
+            password_stored (bool): whether or not the password is stored in
+                keyring
+        """
         result = self.get_keyring_password()
         if result is None:
             print("Password not found.")
@@ -78,6 +96,9 @@ class UserCredentials:
         return password_stored
 
     def set_keyring_password(self, password):
+        """
+        Sets password within keyring
+        """
         if self.enable_keyring:
             keyring.set_password(
                 self.keyring_service_name, self.username, password
